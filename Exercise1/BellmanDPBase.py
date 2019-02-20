69e27f1e93e45eaad4f422cf0ea8bafbce39d442
Esteban <esteban@esrs.co>
2019-02-20 16:20:46 +0000

    DP seems to work.. added plotting file

--- Exercise1/BellmanDPBase.py
+++ Exercise1/BellmanDPBase.py
@@ -1,24 +1,54 @@
 from MDP import MDP
+import numpy as np
+from plotvalue import plot_value_and_policy
 
 class BellmanDPSolver(object):
-	def __init__(self):
-		self.MDP = MDP()
+	def __init__(self, discount_factor):
+		self.discount_factor = discount_factor
+		self.env = MDP()
+		self.initVs()
 
 	def initVs(self):
-		raise NotImplementedError
-
-	# method must return a tuple of (values, policy)
-	def BellmanUpdate(self):
-		a = 10
-		b = 2
-		return(a,b)
-
-		#raise NotImplementedError
+		# init values and policy as dicts with keys = states and values = 0
+		# Values
+		self.V = dict.fromkeys(self.env.S, 0)
+		# Policy
+		self.policy = dict.fromkeys(self.env.S, 0)
 		
+	def BellmanUpdate(self):
+		for s in self.env.S:
+			# Init Action - Value dict with Keys = Actions, Values = values(0)
+			Av = np.zeros(len(self.env.A))
+			c = 0
+			for a in self.env.A:
+				# get transition probability for current action and state
+				t_probs = self.env.probNextStates(s,a).values()
+				# sample given transition probability distribution
+				sample = np.random.choice(len(t_probs),1,p=tuple(t_probs))[0]
+				# get possible next states
+				states = tuple(self.env.probNextStates(s,a).keys())
+				# next state from sample
+				next_state = states[sample]
+				# prob. of chosen action
+				prob = tuple(t_probs)[sample]
+				# fetch reward
+				reward = self.env.getRewards(s, a, next_state)
+				# add to the expectation values
+				Av[c] += prob * (reward + discount_factor * self.V[next_state])
+				c = c+1
+			# find max value and write to dict
+			self.V[s] = np.max(Av)
+			# compute deterministic greedy policy (action that yields more reward) find
+			# index of max action-value in state and write to dict
+			self.policy[s] = self.env.A[np.argmax(Av)]
+		return(self.V,self.policy)
+		# method must return a tuple of (optimal value function, policy)
 
 if __name__ == '__main__':
-	solution = BellmanDPSolver()
-	for i in range(20000):
+	discount_factor = 1
+	solution = BellmanDPSolver(discount_factor)
+	# run n times
+	for i in range(10):
 		values, policy = solution.BellmanUpdate()
 	print("Values : ", values)
 	print("Policy : ", policy)
--- Exercise1/test1.py
+++
@@ -1,9 +0,0 @@
-from MDP import MDP
-
-test1 = MDP()
-# Action DRIBBLE_UP
-# Initstate 0,0
-iS = test1.S[0]
-aC = test1.A[1]
-#print(test1.probNextStates(iS,aC))
-print(aC)
